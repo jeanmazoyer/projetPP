eb334f20d01748dbbc2c9bea770b989b89afde48
Jean Mazoyer <jeanmazoyer@gmail.com>
2014-06-22 17:44:36 +0200

    New name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def home
+  	require 'soundcloud'
+
+# create a client object with your app credentials
+client = Soundcloud.new(:client_id => 'YOUR_CLIENT_ID')
+
+# get a tracks oembed data
+track_url = 'http://soundcloud.com/forss/flickermood'
+embed_info = client.get('/oembed', :url => track_url)
+
+# print the html for the player widget
+puts embed_info['html']
   end
 end
